c9eb60eda97972e4b55738f6bbea081e3323efbc
sudheeshm <sudheeshm@qburst.com>
2019-02-27 19:29:14 +0530

    Fixed the routing issue

--- src/components/App/container.js
+++ src/components/App/container.js
@@ -1,23 +1,25 @@
 import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
 
-import App from './component.jsx';
+import App from './component';
 
 function mapState(state) {
   return {
-     stepDetails: state.app.stepDetails,
-     storyDetails: state.app.storyDetails,
-
+    stepDetails: state.app.stepDetails,
+    storyDetails: state.app.storyDetails,
   };
 }
 
 function mapDispatch(dispatch) {
   // console.log(dispatch.user);
   return {
-   // login: dispatch.user.login,
+    // login: dispatch.user.login,
   };
 }
 
-export default connect(
-  mapState,
-  mapDispatch,
-)(App);
+export default withRouter(
+  connect(
+    mapState,
+    mapDispatch,
+  )(App),
+);
