49ed28211b91e1fac143573adb92e220020abd48
Hanan Nouman <hanan.nouman@consensys.net>
2019-02-28 19:45:58 +0400

    integrate api for first scenario

--- src/components/App/model.js
+++ src/components/App/model.js
@@ -1,11 +1,13 @@
 
 import { red, deepPurple, green, lime, orange, amber  } from '@material-ui/core/colors'
+import request from "@Services/ApiService";
 
 export const initialState = {
   stepDetails: {
     step: 1,
     completed: false,
    },
+   otHash: null,
   // storyDetails: {
   //   entity: 'RERA',
   //   primaryColor: red
@@ -17,21 +19,35 @@ export default {
     updateStep: (state, { step, completed = false }) => ({
       ...state,
       stepDetails: {
-        ...state.stepDetails,
-        step: step || state.step,
+        step: step || state.stepDetails.step,
         completed,
       },
     }),
-    // updateStory: (state, { entity, primaryColor }) => ({
-    //   ...state,
-    //   storyDetails: {
-    //     ...state.storyDetails,
-    //     entity,
-    //     primaryColor,
-    //   },
-    // }),
+    setOtHash: (state, otHash) => ({
+      ...state,
+      otHash
+    }),
     resetApp: () => ({
       ...initialState,
     }),
   },
-};
+  effects: {
+    async getPDF(hash) {
+      try {
+        await request({
+          method: "GET",
+          url: `http://7fe767ba.ngrok.io//doc/${hash}`,
+        });   
+      } catch (error) {
+        console.log(error, 'error');
+      }
+    },
+  },
+}
+
+
+
+
+
+
+
